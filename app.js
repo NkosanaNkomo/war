d51c1b4becbebd603a0e5852c2ccffdd7da93e34
Nkosana Nkomo <dangervybz@gmail.com>
2021-04-14 12:38:15 -0400

    Adding card values

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
-console.log("hello")
-console.log("Hello AGAIN")
+// Clubs Values
+var c2 = 2
+var c3 = 3
+var c4 = 4
+var c5 = 5
+var c6 = 6
+var c7 = 7
+var c8 = 8
+var c9 = 9
+
+// Diamonds Values
+var d2 = 2
+var d3 = 3
+var d4 = 4
+var d5 = 5
+var d6 = 6
+var d7 = 7
+var d8 = 8
+var d9 = 9
+
+// Hearts Values
+var h2 = 2
+var h3 = 3
+var h4 = 4
+var h5 = 5
+var h6 = 6
+var h7 = 7
+var h8 = 8
+var h9 = 9
+
+// Spades Values
+var s2 = 2
+var s3 = 3
+var s4 = 4
+var s5 = 5
+var s6 = 6
+var s7 = 7
+var s8 = 8
+var s9 = 9
+
+// Ace Values
+var aC = 1
+var aD = 1
+var aH = 1
+var aS = 1
