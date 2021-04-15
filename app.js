89baa81b0008fd2d6910d5f0b571a3b677b9e7a1
Nkosana Nkomo <dangervybz@gmail.com>
2021-04-15 11:02:47 -0400

    Changing to new location

--- app.js
+++ app.js
@@ -1,45 +1,57 @@
-// Clubs Values
-var c2 = 2
-var c3 = 3
-var c4 = 4
-var c5 = 5
-var c6 = 6
-var c7 = 7
-var c8 = 8
-var c9 = 9
+// // Clubs Values
+// var c2 = 2
+// var c3 = 3
+// var c4 = 4
+// var c5 = 5
+// var c6 = 6
+// var c7 = 7
+// var c8 = 8
+// var c9 = 9
 
-// Diamonds Values
-var d2 = 2
-var d3 = 3
-var d4 = 4
-var d5 = 5
-var d6 = 6
-var d7 = 7
-var d8 = 8
-var d9 = 9
+// // Diamonds Values
+// var d2 = 2
+// var d3 = 3
+// var d4 = 4
+// var d5 = 5
+// var d6 = 6
+// var d7 = 7
+// var d8 = 8
+// var d9 = 9
 
-// Hearts Values
-var h2 = 2
-var h3 = 3
-var h4 = 4
-var h5 = 5
-var h6 = 6
-var h7 = 7
-var h8 = 8
-var h9 = 9
+// // Hearts Values
+// var h2 = 2
+// var h3 = 3
+// var h4 = 4
+// var h5 = 5
+// var h6 = 6
+// var h7 = 7
+// var h8 = 8
+// var h9 = 9
 
-// Spades Values
-var s2 = 2
-var s3 = 3
-var s4 = 4
-var s5 = 5
-var s6 = 6
-var s7 = 7
-var s8 = 8
-var s9 = 9
+// // Spades Values
+// var s2 = 2
+// var s3 = 3
+// var s4 = 4
+// var s5 = 5
+// var s6 = 6
+// var s7 = 7
+// var s8 = 8
+// var s9 = 9
 
-// Ace Values
-var aC = 1
-var aD = 1
-var aH = 1
-var aS = 1
+// // Ace Values
+// var aC = 1
+// var aD = 1
+// var aH = 1
+// var aS = 1
+
+// var playerOne()=>{
+
+// }
+// var playerTwo()=>{
+
+// }
+
+var suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] 
+
+console.log(suits[3] + values[2])
