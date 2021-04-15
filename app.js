76fe0164a826e98435a12f267c4ba7e6e0d634a8
Nkosana Nkomo <dangervybz@gmail.com>
2021-04-15 11:48:52 -0400

    Card images added

--- app.js
+++ app.js
@@ -52,6 +52,6 @@
 // }
 
 var suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] 
+var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] 
 
 console.log(suits[3] + values[2])
