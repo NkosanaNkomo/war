f5d53cf3e59cbcb121df8d18d365546a6232f4fd
Nkosana Nkomo <dangervybz@gmail.com>
2021-04-19 23:24:26 -0400

    Update

--- app.js
+++ app.js
@@ -53,7 +53,81 @@
 
 import Deck from "./deck.js"
 
+const CARD_VALUE_MAP = {
+    "2" : 2,
+    "3" : 3,
+    "4" : 4,
+    "5" : 5,
+    "6" : 6,
+    "7" : 7,
+    "8" : 8,
+    "9" : 9,
+    "10" : 10,
+    "J" : 11,
+    "Q" : 12,
+    "K" : 13,
+    "A" : 14,
+}
+
+const computerCardSlot = document.querySelector('.computer-card-slot')
+const playerCardSlot = document.querySelector("playerCardSlot")
+const computerDeckElement = document.querySelector(".player-card-slot")
+const playerDeckElement = document.querySelector('.player-deck')
+const text = document.querySelector('.text')
+
+let playerDeck, computerDeck, inRound
+
+document.addEventListener('click', () => {
+    if (inRound) {
+        cleanBeforeRound()
+    }else {
+        flipCards()
+    }
+})
+
+startGame()
+function startGame() {
 const deck = new Deck()
 deck.shuffle()
-console.log(deck.cards)
 
+const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
+playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+inRound = false
+
+cleanBeforeRound()
+
+}
+
+function cleanBeforeRound() {
+    inRound = false
+    computerCardSlot.innerHTML = ''
+    playerCardSlot.innerHTML = ''
+    text.innerText = ''
+
+    updateDeckCount()
+
+}
+
+function flipCards() {
+    inRound = true
+
+    const playerCard = playerDeck.pop()
+    const computerCard = computerDeck.pop()
+
+    playerCardSlot.appendChild(playerCard.getHTML())
+    computerCardSlot.appendChild(computerCard.getHTML())
+
+    updateDeckCount()
+
+
+}
+
+function updateDeckCount() {
+    computerDeckElement.innerText = computerDeck.numberOfCards
+    playerDeckElement.innerText = Deck.numberOfCards
+}
+
+function isRoundWinner(cardOne, CardTwo) {
+    return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value]
+}
--- deck.js
+++ deck.js
@@ -10,6 +10,14 @@ get numberOfCards() {
     return this.cards.length
 }
 
+pop() {
+    return this.cards.shift()
+}
+
+push() {
+    this.cards.push(card)
+}
+
 shuffle() {
     for (let i = this.numberOfCards - 1; i > 0; i--) {
         const newIndex = Math.floor(Math.random() * (i +1))
@@ -26,12 +34,25 @@ class Card {
         this.suit = suit
         this.value = value
     }
+
+get color() {
+    return this.suit === '♧' || this.suit === '♤' ? 'black' : 'red'
+}
+
+getHTML() {
+    const cardDiv = document.createElement('div')
+    cardDiv.innerText = this.suit
+    cardDiv.classList.add("card", this.color)
+    cardDiv.dataset.value = `${this.value} ${this.suit}`
+    return cardDiv
+}
+
 }
 
 function freshDeck() {
-    return SUITS.flatmap(suit => {
-        return VALUES.map(value => {
-            return new Card(suit, value)
+    return SUITS.flatMap(suit => {
+      return VALUES.map(value => {
+        return new Card(suit, value)
         })
     })
 }
